3e5e5efdcc63daa37597d7a66ebebd9466ed0aec
Alex Diaz(He/Hiim)(Gilroy) <alexander.adolfo.diaz017@gmail.com>
2025-10-01 10:13:49 -0700

    MMA exercise 1 for chatGPT

---
+++ demo-Project/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int opponentHealth = 100;
+
+        opponentHealth = opponentHealth - 30;
+        System.out.println("Opponent's health is now: " + opponentHealth);
+    }
+}
