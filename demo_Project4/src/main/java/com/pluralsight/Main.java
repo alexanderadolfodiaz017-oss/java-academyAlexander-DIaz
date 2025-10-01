d36e05ea39b26a84b679d8f4e17d06a7f489c0f6
Alex Diaz(He/Hiim)(Gilroy) <alexander.adolfo.diaz017@gmail.com>
2025-10-01 11:10:14 -0700

    Exercise 4 for Chatgppt demo

---
+++ demo_Project4/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Punch " + i + " lands!");
+        }
+   //What will print on the screen when you run this loop?
+                //Punch 1 lands!
+                //Punch 3 lands!
+                //Punch 4 lands!
+                //Punch 5 lands!
+                //Punch 6 lands!
+                //Punch 7 lands!
+                //Punch 8 lands!
+                //Punch 9 lands!
+                //Punch 10 lands!
+    }
+}
