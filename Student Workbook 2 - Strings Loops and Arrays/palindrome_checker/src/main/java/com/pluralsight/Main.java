3c785e6e67cf809e750b9f00a10c193027ced0a5
Alex Diaz(He/Hiim)(Gilroy) <alexander.adolfo.diaz017@gmail.com>
2025-10-03 01:42:24 -0700

    Add files via upload

---
+++ Student Workbook 2 - Strings Loops and Arrays/palindrome_checker/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        System.out.println(word.equals(reversed));
+
+        scanner.close();
+    }
+}
---
+++ Student Workbook 2 - Strings Loops and Arrays/palindrome_checker/src/main/java/com/pluralsight/palindrome_checker.java
@@ -0,0 +1,4 @@
+package com.pluralsight;
+
+public class palindrome_checker {
+}
