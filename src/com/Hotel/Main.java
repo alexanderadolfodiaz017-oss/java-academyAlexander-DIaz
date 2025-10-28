b44c9d793ea06b82ac9d9c80d10e378abada4026
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-10-28 14:03:27 -0700

    Add Hotel project files

---
+++ src/com/Hotel/Main.java
@@ -0,0 +1,7 @@
+package com.Hotel;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/Hotel/Room.java
@@ -0,0 +1,49 @@
+package com.Hotel;
+
+public class Room {
+//..
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+}
